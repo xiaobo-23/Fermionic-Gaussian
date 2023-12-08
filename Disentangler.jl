cfae36d90276c91f6346a897ea860165b6d8cd13
Bo Xiao <bxiao@flatironinstitute.org>
2023-12-08 11:45:31 -0500

    Optimize a sequence of gates using the Evenly-Vidal algorithm

--- Disentangler.jl
+++ Disentangler.jl
@@ -4,7 +4,6 @@
 using Pkg
 using ITensors
 using HDF5
-# using Distributions
 using Random 
 using NLopt
 
@@ -21,7 +20,7 @@ let
 
     # Obtain the ground state of the Heisenberg model
     #************************************************************************************
-    N = 100
+    N = 5
     sites = siteinds("S=1/2", N)
 
     os = OpSum()
@@ -47,15 +46,15 @@ let
 
     # The parameters to be optimized
     # Initialize the parameters to be one
-    # alpha = ones(subsystem_size - 1, number_of_generators)    
+    alpha = ones(subsystem_size - 1, number_of_generators)    
     
     # Random initialization of the parameters
-    random_seed=0
-    Random.seed!(random_seed)
-    upper_bound = 5.0
-    lower_bound = -5.0
-    alpha = rand(subsystem_size - 1, number_of_generators) * (upper_bound - lower_bound) .+ lower_bound     # The parameters to be optimized
-    @show alpha
+    # random_seed=0
+    # Random.seed!(random_seed)
+    # upper_bound = 5.0
+    # lower_bound = -5.0
+    # alpha = rand(subsystem_size - 1, number_of_generators) * (upper_bound - lower_bound) .+ lower_bound     # The parameters to be optimized
+    # @show alpha
     # @show typeof(alpha)
 
     # Define the generators of SU(4) group
@@ -81,14 +80,14 @@ let
         s2 = sites[index + 1]
         tmp_gate = itensor(exp(im * tmp_matrix), s1', s2', s1, s2)
 
-        @show typeof(tmp_gate)
+        # @show typeof(tmp_gate)
         push!(gates, tmp_gate)
     end
 
     # Apply the generalized gates to the MPS
     ψ_copy = deepcopy(ψ)
     SvN[1, :] = entanglement_entropy(ψ_copy, N)
-    Sz[1, :] = expect(ψ_copy, "Sz"; sites = 1 : N) 
+    Sz[1, :] = expect(ψ_copy, "Sz"; sites = 1 : N)
     
     ψ_copy = apply(gates, ψ_copy; cutoff = 1e-8)
     Sz[2, :] = expect(ψ_copy, "Sz"; sites = 1 : N)
@@ -102,11 +101,33 @@ let
     @show SvN[1, :]
     @show SvN[2, :]
 
-    h5open("disentangler_N100_random$(random_seed).h5", "w") do file
-        write(file, "psi", ψ_copy)
-        write(file, "SvN", SvN)
-        write(file, "Sz", Sz)
-    end
+
+    ## 12/06/2023
+    # Implement the optimization process for the first gate in a reverse order 
+    psi_bra = deepcopy(ψ)
+    psi_ket = deepcopy(ψ)
+    
+    # Apply all the gates in the original order
+    @show siteinds(psi_ket)
+    psi_ket = apply(gates, psi_ket; cutoff = 1e-8)
+    @show siteinds(psi_ket)
+
+    # Apply all the gates except for the gate that is being optimized
+    reverse_gates = reverse(gates)
+    psi_ket = apply(reverse_gates[1 : subsystem_size - 2], psi_ket; cutoff = 1e-8)
+    @show length(reverse_gates)
+    
+    # @show gates, reverse_gates
+    # @show siteinds(psi_ket)
+    # @show siteinds(psi_bra)
+    @show length(psi_bra), length(psi_ket)
+    @show inner(psi_bra, psi_ket)
+
+    # h5open("Data/disentangler_N100_random$(random_seed).h5", "w") do file
+    #     write(file, "psi", ψ_copy)
+    #     write(file, "SvN", SvN)
+    #     write(file, "Sz", Sz)
+    # end
 
     return
 end
